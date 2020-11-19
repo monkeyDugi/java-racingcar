0d83e6a5ed1594d18f2c91ec71e5aa8e678b1151
monkeyDugi <lbd4946@gmail.com>
2020-11-19 21:12:29 +0900

    refactor : controller 구현

--- src/main/java/racing/controller/RacingController.java
+++ src/main/java/racing/controller/RacingController.java
@@ -1,2 +1,18 @@
-package racing.controller;public class RacingController {
+package racing.controller;
+
+import racing.domain.RacingGame;
+import racing.domain.RacingRound;
+import racing.domain.RacingWinner;
+import racing.ui.ResultView;
+
+public class RacingController {
+
+    public void race(String[] names, int roundCount) {
+        RacingGame racingGame = new RacingGame(roundCount, names);
+        RacingRound racingRound = racingGame.startRacing();
+        RacingWinner racingWinner = new RacingWinner(racingRound);
+
+        ResultView.output(racingRound, racingWinner);
+    }
+
 }
